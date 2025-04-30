a67b339530a5e8b46cf0346601e1eb8b22af9ba2
rom-ann <146835440+romannmaypbalingbing@users.noreply.github.com>
2025-04-30 22:43:47 +0800

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 // This file is the main entry point of the Flutter application.
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:vitawatch/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
